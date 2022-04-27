96c1e666da5b429bdcaad62827cf39fa3907ee33
Piotr Gągorowski <236533@edu.p.lodz.pl>
2022-04-27 16:44:48 +0200

    Pierwszy

---
+++ main.py
@@ -0,0 +1,11 @@
+# dodac funkcje ktore beda dzialac i dla kontrastu te ktore nie beda (nie mają pochodnej, czy cos)
+# -M <= |f(x)| <= M  ##### jakis wazny warunek
+# funckje wymierne nadają sie do przykladow negatywnych
+# trzymaj sie instrukcji bo cie Runge zmiecie z planszy
+# uruchomic pare razy, wyniki sie powinny powtarzac
+def main():
+    print("test")
+
+
+if __name__ == "__main__":
+    main()
