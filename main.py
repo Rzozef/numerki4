c0b927d6e61c81b78c4848ab5294329a59627750
Piotr Gągorowski <236533@edu.p.lodz.pl>
2022-05-03 20:02:41 +0200

    usuniete komentarze

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# dodac funkcje ktore beda dzialac i dla kontrastu te ktore nie beda (nie mają pochodnej, czy cos)
-# funckje wymierne nadają sie do przykladow negatywnych
-# trzymaj sie instrukcji bo cie Runge zmiecie z planszy
-# uruchomic pare razy, wyniki sie powinny powtarzac
-
-import math
 import matplotlib.pyplot as pyplot
 import numpy as np
 
